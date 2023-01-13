8226d4740019a02bdea3573dc6c2430dc6d2e927
Daniel Aguiar <dc03934@gmail.com>
2023-01-13 07:39:59 -0300

    Edited

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { HomeScreen } from './src/components/screens';
+
+import appConfig from './config/app.config';
+import Navigation from './src/components/modules/Navigation';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar style={appConfig.statusBarStyle} />
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Navigation" 
+          component={Navigation} 
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="HomeScreen" 
+          component={HomeScreen} 
+          options={{ headerShown: false }} 
+        />
+      </Stack.Navigator>
+      {/* <Navigation/> */}
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
